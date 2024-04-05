dec9cb65eccdeb0439dbee13769a58505aa8bc41
Anas099X <Anasshohdy400@gmail.com>
2024-04-05 15:25:41 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from g4f.client import Client
+from insert_questions import add_question
+
+promptfile = open("prompt_input.txt", "r+")
+importfile = open("input.txt", "r+")
+outputfile = open("output.json", "w+")
+
+client = Client()
+response = client.chat.completions.create(
+    model="gpt-4-turbo",
+    messages=[{"role": "user", "content": promptfile.read() + "\n" + importfile.read()}],
+    
+)
+outputfile.write(response.choices[0].message.content)
+add_question("sat_question_test","questions",response.choices[0].message.content)
+#print(response.choices[0].message.content)
+
