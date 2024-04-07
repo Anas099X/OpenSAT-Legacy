11044262a7d699d954b48b1d5ea99c6249478a5a
Anas099X <Anasshohdy400@gmail.com>
2024-04-07 17:15:26 +0300

    fixed AI prompt

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
+from g4f.client import Client
 from PyPDF2 import PdfReader
 
+client = Client()
 reader = PdfReader("test.pdf")
-page = reader.pages[0]
-print("     ")
-print(page.extract_text())
+
+def send_message(message):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}]
+    )
+    return response.choices[0].message.content
+
+page = reader.pages
+for i in page:
+    message = f"turn following text into json: {i}"
+    response = send_message(message)
+    print(f"Response: {response}")
