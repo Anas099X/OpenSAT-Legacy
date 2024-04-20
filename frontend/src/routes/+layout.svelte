d1f31516b647a9511b09898cdef988233f280623
Anas099X <Anasshohdy400@gmail.com>
2024-04-20 10:23:17 +0300

    added searchbar and grid view

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,4 +2,18 @@
 	import '../app.postcss';
 </script>
 
-<slot />
+<div class="mainlayout">
+	<slot />
+  </div>
+  
+  <style>
+	.mainlayout {
+	  height: 100vh; /* Set height to 100 viewport height */
+	  width: 100%;
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  position: relative;
+	}
+  </style>
+  
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-  import { AppBar, AppShell, TreeView, TreeViewItem } from '@skeletonlabs/skeleton';
+  import { AppBar, AppRail, AppRailAnchor, AppRailTile, AppShell, TreeView, TreeViewItem } from '@skeletonlabs/skeleton';
   
   import { onMount, onDestroy } from 'svelte';
   
   let question_data: any;
+  let currentTile: number = 0;
 
   
   let question_db_input: string = "sat_question_test"; // Set a specific type (string)
@@ -35,28 +36,47 @@
   
 
     
-  <AppShell>
-    <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
-  
-    <div style="position: absolute; top: 20%; left: 50%; transform: translate(-50%, -20%); margin-bottom: 20%; width:50%">
-      <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-        <div class="input-group-shim">(icon)</div>
-        <input type="search" placeholder="Search..." />
-        <button class="variant-filled-secondary">Submit</button>
-      </div>
-    </div>
-  
-    <div class="card p-4 overflow-y-auto" style="height: 400px; width: 600px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">  
+  <AppShell slotSidebarLeft="h-auto">
+    <svelte:fragment slot="sidebarLeft">
+      <AppRail height='h-full' gap='gap-1'>
+        <svelte:fragment slot="lead">
+		<AppRailAnchor href="/" >(icon)</AppRailAnchor>
+	</svelte:fragment>
+	<!-- --- -->
+	<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<span>Tile 1</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<span>Tile 2</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<span>Tile 3</span>
+	</AppRailTile>
+	<!-- --- -->
+	<svelte:fragment slot="trail">
+		<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+	</svelte:fragment>
+        </AppRail>
+    </svelte:fragment>
+    <input class="input" title="Input (text)" type="text" placeholder="input text" style="position: absolute; top: 20%; left: 18%; transform: translate(-20%, -18%); width:300px; margin-bottom: 2%;" />
+    <div class="card-container p-4 overflow-y-auto !bg-transparent" style="height: 400px; width: 1100px; position: absolute; top: 45%; left: 35%; transform: translate(-30%, -30%);">
       {#if isFetchingData}
         <p>Loading data...</p>
       {:else if question_data}
-        {#each question_data as x}
-          <ol class="list">
-            <li>
-              <span class="flex-auto">{x.question.paragraph}</span>
-            </li>
-          </ol>
-        {/each}
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-4">  
+          {#each question_data as x}
+            <div class="card p-4" style="height:20vh">
+              <section class="p-2">
+                <h class="h4">English Section</h>
+              </section>
+              <footer class="p-4 flex justify-start items-center space-x-4"><small>Question #{x.id}</small></footer>
+            </div>
+            
+          {/each}
+        </div>
       {:else}
         <p>No data available yet.</p>
       {/if}
