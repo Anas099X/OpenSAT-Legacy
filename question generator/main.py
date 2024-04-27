89b8f3f468bd755b4aeaa0c9cd9dc0da088b3f3e
Anas099X <Anasshohdy400@gmail.com>
2024-04-27 13:44:26 +0300

    updated the AI provider and model

--- question generator/main.py
+++ question generator/main.py
@@ -1,28 +1,30 @@
 from g4f.client import Client
+import g4f
 from insert_questions import add_question
 from PyPDF2 import PdfReader
 import time
 
 client = Client()
-reader = PdfReader("Information_and_Ideas.pdf")
+reader = PdfReader("main_input.pdf")
 
 
 #import prompt for instuctions
-promptfile = open("reformat_prompt.txt", "r+").read()
+promptfile = open("question generator/reformat_prompt.txt", "r+").read()
 
 #importfile = open("input.txt", "r+")
-outputfile = open("internal_db/Information_and_Ideas.json", "w+")
+outputfile = open("internal_db/SAT_database.json", "w+")
 
 #auto question converter and exporter
 sat_pdf = reader.pages
 for page in sat_pdf:
  response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model="gpt-4",
     messages=[{"role": "user", "content": promptfile + "\n" + page.extract_text()}],
+    provider=g4f.Provider.Liaobots
    )
  try:
-  outputfile.write(response.choices[0].message.content + ',')
   add_question("sat_database","english",response.choices[0].message.content)
+  outputfile.write(response.choices[0].message.content + ',')
   time.sleep(0.55)
   print("Question Added!")
   print(response.choices[0].message.content)
